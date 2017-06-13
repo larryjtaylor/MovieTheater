414a73e12fa22b03006cb3894af785caad2725a2
Larry Taylor and Michelle Poterek <larry@>
2017-06-13 13:10:36 -0700

    add select box options

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// business logic
+
+function Ticket = (movie, time, age) {
+this.movieName = movie;
+this.movieTime = time;
+this.userAge = age;
+}
+
+
+
+// UI logic
+$(document).ready(function(){
+
+});
