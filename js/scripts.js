cd169b996969de10d087f66da833234bd6e19963
Larry Taylor and Michelle Poterek <michelle@>
2017-06-13 15:07:57 -0700

    show price of each ticket type and total price

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,27 @@
 // business logic
 
-function Ticket = (movie, time, age) {
+function Ticket (movie, time, age) {
 this.movieName = movie;
 this.movieTime = time;
 this.userAge = age;
 }
 
-
-
+// function showTickets(){
+//
+// }
 // UI logic
 $(document).ready(function(){
 
+  $(".see-movie").submit(function(event) {
+    console.log("working");
+    event.preventDefault();
+    var inputAdult = document.getElementById("numberAdult").value;
+
+    var inputChild = document.getElementById("numberChild").value;
+
+    var inputSenior = document.getElementById("numberSenior").value;
+
+    var total = "Total: " + (Number(inputAdult) + Number(inputChild) + Number(inputSenior));
+    $("#results").append("<li>" + inputAdult + "</li>" + "<li>" + inputChild + "</li>" + "<li>" + inputSenior + "</li>" + "<li>" + total + "</li>")
+  });
 });
